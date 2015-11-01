bc400d9b4e6690e23ed34fb1ad414c7fc94c8cd5
jstewart983 <nj.jstewart@gmail.com>
2015-11-01 00:38:06 -0500

    rs site

---
+++ scripts/wowinit.js
@@ -0,0 +1,12 @@
+angular.module('wow', [])
+
+.directive('toolbarTip', function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element, attrs) {
+             new WOW().init();
+        }
+    };
+});
