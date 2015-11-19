8674534bde0d50a97f9601d082054f2f2dc8cdd9
jstewart983 <nj.jstewart@gmail.com>
2015-11-18 23:58:49 -0600

    added shadow to navbar when scrolling down

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
 angular.module('rsapp', ['ngAnimate','ngTouch'])
 .controller('websiteCtrl', [function() {
   new WOW().init();
-
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#header").addClass("active");
+    }
+    else {
+        $("#header").removeClass("active");
+    }
+});
 
 }]);
