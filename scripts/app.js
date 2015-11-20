8122a27add14cc3e73062cb7f5f99aaa42b6a3a0
Jordan Stewart <nj.jstewart@gmail.com>
2015-11-20 13:19:38 -0600

    added images and started work section

--- scripts/app.js
+++ scripts/app.js
@@ -27,7 +27,7 @@ angular.module('rsapp', [])
     ///Boolean - Whether grid lines are shown across the chart
 scaleShowGridLines : false,
 
-responsive: true,//
+responsive: true,
 
 //String - Colour of the grid lines
 scaleGridLineColor : "#fff",
