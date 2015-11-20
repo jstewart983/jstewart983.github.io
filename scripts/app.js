6ded1b874dae4be341575a10a1d95cfbc7b330d7
jstewart983 <nj.jstewart@gmail.com>
2015-11-19 18:31:05 -0600

    added angular chart to bottom of page

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,25 @@
-angular.module('rsapp', ['ngAnimate'])
+angular.module('rsapp', ['ngAnimate','chart.js'])
+
+.config(['ChartJsProvider', function (ChartJsProvider) {
+  // Configure all charts
+  ChartJsProvider.setOptions({
+    colours: ['#E74C3C', '#E74C3C'],
+    responsive: true
+  });
+}])
+.controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["", "", "", "", "", "", ""];
+  $scope.series = [''];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+})
+
 .controller('websiteCtrl', [function() {
   new WOW().init();
   $(window).scroll(function() {
