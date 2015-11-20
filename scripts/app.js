cd0d8dfac932c80071d0010298f31cc9363e9e51
jstewart983 <nj.jstewart@gmail.com>
2015-11-19 19:21:21 -0600

    fix

--- scripts/app.js
+++ scripts/app.js
@@ -17,8 +17,8 @@ angular.module('rsapp', [])
           }
       ]
   };
-  var myLineChart = new Chart(ctx).Line(data, options);
   var ctx = document.getElementById("myChart").getContext("2d");
+  var myLineChart = new Chart(ctx).Line(data, options);
 })
 
 
